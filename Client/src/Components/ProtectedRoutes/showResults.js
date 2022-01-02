91c63ebc2315ee9617d7bf8f5fd51f3fa7bc6732
Gulshan Aggarwal <gulshanaggarwal333@gmail.com>
2022-01-02 11:20:17 +0530

    approx 7 hostels data integrated

--- Client/src/Components/ProtectedRoutes/showResults.js
+++ Client/src/Components/ProtectedRoutes/showResults.js
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 import { useDispatch, useSelector, shallowEqual } from "react-redux"
 import { setResidencyData } from '../../features/residencyDataSlice';
 import rupeeIcon from "../../Images/rupee-indian.png";
-import {showBookingForm} from "../../features/bookingSlice"
+import { showBookingForm } from "../../features/bookingSlice"
 
 
 export default function ShowResults() {
@@ -33,7 +33,7 @@ export default function ShowResults() {
                 console.log(ele.images[0].url);
                 return (
                     <div className='w-90P mx-auto my-8 border-1 border-gray-600 rounded-md bg-gray-100' key={ele._id}>
-                        <div className="w-full h-52">
+                        <div className="w-full h-64">
                             <img src={ele.images[0].url} className='w-full h-full rounded-md' alt={ele.images[0].description} />
                         </div>
                         <div className='px-3 py-5'>
@@ -47,13 +47,18 @@ export default function ShowResults() {
                                         <p className='px-2'>{ele.location}</p>
                                     </div>
                                 </div>
-                                {ele.residencyFor === "Boys" ? <div className="flex p-2"><svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6' viewBox="0 0 24 24"><circle cx="12" cy="4" r="2"></circle><path d="M15 7H9a1 1 0 0 0-1 1v7h2v7h4v-7h2V8a1 1 0 0 0-1-1z"></path></svg><p>Boys</p></div> : <div className='flex'><svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' viewBox="0 0 24 24"><circle cx="12" cy="4" r="2"></circle><path d="M14.948 7.684A.997.997 0 0 0 14 7h-4a.998.998 0 0 0-.948.684l-2 6 1.775.593L8 18h2v4h4v-4h2l-.827-3.724 1.775-.593-2-5.999z"></path></svg><p>Girls</p></div>}
+                                {ele.residencyType.hostel.length > 0 && (ele.residencyFor === "Boys" ? <div className="flex p-2"><svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6' viewBox="0 0 24 24"><circle cx="12" cy="4" r="2"></circle><path d="M15 7H9a1 1 0 0 0-1 1v7h2v7h4v-7h2V8a1 1 0 0 0-1-1z"></path></svg><p>Boys</p></div> : <div className='flex'><svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' viewBox="0 0 24 24"><circle cx="12" cy="4" r="2"></circle><path d="M14.948 7.684A.997.997 0 0 0 14 7h-4a.998.998 0 0 0-.948.684l-2 6 1.775.593L8 18h2v4h4v-4h2l-.827-3.724 1.775-.593-2-5.999z"></path></svg><p>Girls</p></div>)}
                             </div>
                             {
-                                ele.residencyType.hostel.length > 0 && <div className='flex align-middle my-2'>
+                                ele.residencyType.hostel.length > 0 ? <div className='flex align-middle my-2'>
                                     <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' viewBox="0 0 24 24"><path d="M20 9.556V3h-2v2H6V3H4v6.557C2.81 10.25 2 11.526 2 13v4a1 1 0 0 0 1 1h1v4h2v-4h12v4h2v-4h1a1 1 0 0 0 1-1v-4c0-1.474-.811-2.75-2-3.444zM11 9H6V7h5v2zm7 0h-5V7h5v2z"></path></svg>
                                     {
-                                        ele.residencyType.hostel.map((item,index) => <span key={index} className='px-1'>{item.roomType},</span>)
+                                        ele.residencyType.hostel.map((item, index) => <span key={index} className='px-1'>{item.roomType},</span>)
+                                    }
+                                </div> : <div className='flex align-middle my-2'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' viewBox="0 0 24 24"><path d="M20 9.556V3h-2v2H6V3H4v6.557C2.81 10.25 2 11.526 2 13v4a1 1 0 0 0 1 1h1v4h2v-4h12v4h2v-4h1a1 1 0 0 0 1-1v-4c0-1.474-.811-2.75-2-3.444zM11 9H6V7h5v2zm7 0h-5V7h5v2z"></path></svg>
+                                    {
+                                        ele.residencyType.flat.map((item, index) => <span key={index} className='px-1'>{item.roomType},</span>)
                                     }
                                 </div>
                             }
@@ -70,7 +75,7 @@ export default function ShowResults() {
                                         <span className='px-2'>{ele.startingPrice}/month</span>
                                     </div>
                                 </div>
-                                <button type="button" onClick={()=>dispatch(showBookingForm({data:{residencyName:ele.name,residencyType:ele.residencyType}}))} className='rounded-md w-32 h-12 bg-indigo-400 text-sm text-white font-medium'>Book now</button>
+                                <button type="button" onClick={() => dispatch(showBookingForm({ data: { residencyName: ele.name, residencyType: ele.residencyType } }))} className='rounded-md w-32 h-12 bg-indigo-400 text-sm text-white font-medium'>Book now</button>
                             </div>
                         </div>
                     </div>
