7c740743a0c6c081cbba17f4d4fe6618e4ddcf3e
Gulshan Aggarwal <gulshanaggarwal333@gmail.com>
2022-01-28 10:05:31 +0530

    profile page error resolved

--- Client/src/Components/commonHeader/protectedPage.js
+++ Client/src/Components/commonHeader/protectedPage.js
@@ -35,8 +35,9 @@ export default function ProtectedPageHeader() {
                         </svg>
                         <svg className={`h-1 w-1 transform ${rotation} sm:h-3 sm:w-3`} viewBox="0 0 8 5" fill="white" xmlns="http://www.w3.org/2000/svg"><path d="M1.414 0C.524 0 .077 1.077.707 1.707l2.586 2.586a1 1 0 001.414 0l2.586-2.586C7.923 1.077 7.477 0 6.586 0H1.414z"></path></svg>
                     </div>
-                    <nav className={`absolute text-xs right-1 mt-7 bg-white rounded-md z-50 ${nav} shadow-2xl sm:text-base`}>
+                    <nav className={`absolute text-xs right-1 mt-4 bg-white rounded-md z-50 ${nav} shadow-2xl sm:text-base`}>
                         <ul className='py-2'>
+                            <Link className='w-full' to="/"><li className='pl-2 pr-8 py-2 hover:bg-gray-200'>Go Home</li></Link>
                             <Link className='w-full' to="/dashboard"><li className='pl-2 pr-8 py-2 hover:bg-gray-200'>Dashboard</li></Link>
                             <Link className='w-full' to="/profile"><li className='pl-2 pr-8 py-2 hover:bg-gray-200'>Profile</li></Link>
                             <li className='pl-2 pr-8 py-2 hover:bg-gray-200 cursor-pointer' onClick={handleLogout}><button type='button'>Logout</button></li>
